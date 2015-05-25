f334b990c9a4646ec627cbb613ae3e0ec4c0d0b2
decrula <decrula@gmail.com>
2015-05-26 01:58:04 +0800

    解决IE浏览器兼容模式下，无法弹出趋势的问题。

--- Monitor_shell/Monitor_shell.Web/UI_Monitor/js/common/chartjs.js
+++ Monitor_shell/Monitor_shell.Web/UI_Monitor/js/common/chartjs.js
@@ -1,6 +1,12 @@
 ﻿$(function () {
-    $(".mchart").click(function () {
-        var url = "/UI_Monitor/TrendTool/TrendlineRenderer.aspx#" + this.id;
-        window.open(url, "WindowChart", "width=800,height=600,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no");
-    })
+    if ($.browser.msie) {
+        $(".mchart").click(function () {
+            window.open("/UI_Monitor/TrendTool/TrendlineRenderer.aspx#" + this.id, "_blank", "width=800,height=600,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no");
+        })
+    }
+    else {
+        $(".mchart").click(function () {
+            window.open("/UI_Monitor/TrendTool/TrendlineRenderer.aspx#" + this.id, this.id, "width=800,height=600,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no");
+        })
+    }
 })
